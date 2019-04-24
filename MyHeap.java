856ef998d423d75d48f1dc65f200dfac1c2c7aa6
bertw2002 <awan00@stuy.edu>
2019-04-23 20:24:44 -0400

    some code

--- MyHeap.java
+++ MyHeap.java
@@ -1,1 +1,19 @@
+public class MyHeap{
+  private static void swap(int[] data, int first, int second){
+    int newvar = data[first];
+    data[first] = data[second];
+    data[second] = newvar;
+  }
+  private static void pushDown(int[] data, int size, int index){
 
+  }
+  private static void pushUp(int[]data, int index){
+
+  }
+  public static void heapify(int[] arr){
+
+  }
+  public static void heapsort(int[] arr){
+
+  }
+}
